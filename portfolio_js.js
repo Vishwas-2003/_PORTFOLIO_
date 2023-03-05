b4efc4486503f479ee50cb75aa53cc5d98f0fdc0
Vishwas Vijay Vargiya <vijayvargiyavishwas@gmail.com>
2023-03-05 20:13:58 +0530

    initial commit

---
+++ portfolio_js.js
@@ -0,0 +1,186 @@
+function load(){
+    document.getElementById("loading").style.display="none";
+}
+function about(){
+    document.getElementById("about_write").style.display="block";
+    document.getElementById("about_write_ul").style.display="block";
+    document.getElementById("about").style.border="dotted";
+    document.getElementById("about").style.background="#e6c8f0";
+    
+    document.getElementById("name_write").style.display="none";
+    document.getElementById("education_write").style.display="none";
+    document.getElementById("certification_write").style.display="none";
+    document.getElementById("projects_write").style.display="none";
+    document.getElementById("contact_write").style.display="none";
+    document.getElementById("certification_writeul").style.display="none";
+    document.getElementById("projects_writeul").style.display="none";
+
+    
+    document.getElementById("name").style.border="solid";
+    document.getElementById("education").style.border="solid";
+    document.getElementById("certification").style.border="solid";
+    document.getElementById("projects").style.border="solid";
+    document.getElementById("contact").style.border="solid";
+    
+    document.getElementById("name").style.background="#E5B8F4";
+    document.getElementById("education").style.background="#E5B8F4";
+    document.getElementById("certification").style.background="#E5B8F4";
+    document.getElementById("projects").style.background="#E5B8F4";
+    document.getElementById("contact").style.background="#E5B8F4";
+}
+function education(){
+    document.getElementById("education_write").style.display="block";
+    document.getElementById("education").style.border="dotted";
+    document.getElementById("education").style.background="#e6c8f0";
+    
+    document.getElementById("about_write").style.display="none";
+    document.getElementById("about_write_ul").style.display="none";
+    document.getElementById("name_write").style.display="none";         
+    document.getElementById("certification_write").style.display="none";
+    document.getElementById("projects_write").style.display="none";
+    document.getElementById("contact_write").style.display="none";
+    document.getElementById("certification_writeul").style.display="none";
+    document.getElementById("projects_writeul").style.display="none";
+
+    
+    document.getElementById("name").style.border="solid";
+    document.getElementById("about").style.border="solid";
+    document.getElementById("certification").style.border="solid";
+    document.getElementById("projects").style.border="solid";
+    document.getElementById("contact").style.border="solid";
+    
+    document.getElementById("name").style.background="#E5B8F4";
+    document.getElementById("about").style.background="#E5B8F4";
+    document.getElementById("certification").style.background="#E5B8F4";
+    document.getElementById("projects").style.background="#E5B8F4";
+    document.getElementById("contact").style.background="#E5B8F4";
+}
+function Name(){
+    document.getElementById("name_write").style.display="block";
+    document.getElementById("name").style.border="dotted";
+    document.getElementById("name").style.background="#e6c8f0";
+
+    document.getElementById("about_write").style.display="none";
+    document.getElementById("about_write_ul").style.display="none";
+    document.getElementById("education_write").style.display="none";
+    document.getElementById("certification_write").style.display="none";
+    document.getElementById("projects_write").style.display="none";
+    document.getElementById("contact_write").style.display="none";
+    document.getElementById("certification_writeul").style.display="none";
+    document.getElementById("projects_writeul").style.display="none";
+    
+    document.getElementById("about").style.border="solid";
+    document.getElementById("education").style.border="solid";
+    document.getElementById("certification").style.border="solid";
+    document.getElementById("projects").style.border="solid";
+    document.getElementById("contact").style.border="solid";
+    
+    document.getElementById("about").style.background="#E5B8F4";
+    document.getElementById("education").style.background="#E5B8F4";
+    document.getElementById("certification").style.background="#E5B8F4";
+    document.getElementById("projects").style.background="#E5B8F4";
+    document.getElementById("contact").style.border="solid";
+
+}
+function certification(){
+    document.getElementById("certification_write").style.display="block";
+    document.getElementById("certification_writeul").style.display="block";
+    document.getElementById("certification").style.border="dotted";
+    document.getElementById("certification").style.background="#e6c8f0";
+
+    
+    document.getElementById("name_write").style.display="none";
+    document.getElementById("about_write").style.display="none";
+    document.getElementById("about_write_ul").style.display="none";
+    document.getElementById("education_write").style.display="none";
+    document.getElementById("projects_write").style.display="none";
+    document.getElementById("projects_writeul").style.display="none";
+    document.getElementById("contact_write").style.display="none";
+    
+    document.getElementById("name").style.border="solid";
+    document.getElementById("education").style.border="solid";
+    document.getElementById("about").style.border="solid";
+    document.getElementById("projects").style.border="solid";
+    document.getElementById("contact").style.border="solid";
+    
+    document.getElementById("name").style.background="#E5B8F4";
+    document.getElementById("education").style.background="#E5B8F4";
+    document.getElementById("about").style.background="#E5B8F4";
+    document.getElementById("projects").style.background="#E5B8F4";
+    document.getElementById("contact").style.background="#E5B8F4";
+}
+function projects(){
+    document.getElementById("projects_write").style.display="block";
+    document.getElementById("projects_writeul").style.display="block";
+    document.getElementById("projects").style.border="dotted";
+    document.getElementById("projects").style.background="#e6c8f0";
+
+    
+    document.getElementById("name_write").style.display="none";
+    document.getElementById("about_write").style.display="none";
+    document.getElementById("about_write_ul").style.display="none";
+    document.getElementById("certification_write").style.display="none";
+    document.getElementById("certification_writeul").style.display="none";
+    document.getElementById("education_write").style.display="none";
+    document.getElementById("contact_write").style.display="none";
+    
+    document.getElementById("name").style.border="solid";
+    document.getElementById("education").style.border="solid";
+    document.getElementById("certification").style.border="solid";
+    document.getElementById("about").style.border="solid";
+    document.getElementById("contact").style.border="solid";
+    
+    document.getElementById("name").style.background="#E5B8F4";
+    document.getElementById("education").style.background="#E5B8F4";
+    document.getElementById("certification").style.background="#E5B8F4";
+    document.getElementById("about").style.background="#E5B8F4";
+    document.getElementById("contact").style.background="#E5B8F4";
+}
+function contact(){
+    document.getElementById("contact_write").style.display="block";
+    document.getElementById("contact").style.border="dotted";
+    document.getElementById("contact").style.background="#e6c8f0";
+
+    
+    document.getElementById("name_write").style.display="none";
+    document.getElementById("about_write").style.display="none";
+    document.getElementById("about_write_ul").style.display="none";
+    document.getElementById("education_write").style.display="none";
+    document.getElementById("certification_write").style.display="none";
+    document.getElementById("certification_writeul").style.display="none";
+    document.getElementById("projects_writeul").style.display="none";
+    document.getElementById("projects_write").style.display="none";
+    
+    document.getElementById("name").style.border="solid";
+    document.getElementById("education").style.border="solid";
+    document.getElementById("certification").style.border="solid";
+    document.getElementById("projects").style.border="solid";
+    document.getElementById("about").style.border="solid";
+    
+    document.getElementById("name").style.background="#E5B8F4";
+    document.getElementById("education").style.background="#E5B8F4";
+    document.getElementById("certification").style.background="#E5B8F4";
+    document.getElementById("projects").style.background="#E5B8F4";
+    document.getElementById("about").style.background="#E5B8F4";
+}
+function disnav(){
+    document.getElementById("menu").style.display="block";
+    document.getElementById("nonavi").style.display="block";
+    document.getElementById("menu").style.zIndex="1";
+    document.getElementById("menu").style.position="absolute";
+    document.getElementById("navi").style.display="none";
+}
+function exit(){
+    document.getElementById("navi").style.display="block";
+    document.getElementById("menu").style.display="none";
+    document.getElementById("nonavi").style.display="none";
+}
+function reset(){
+    if(window.innerWidth>1239){
+        document.getElementById("menu").style.display="block";
+        document.getElementById("menu").style.position="static";
+    }
+    else if(window.innerWidth<1239){
+        exit();
+    }
+}
